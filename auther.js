9a5efe8d8a29446f31fc7c36f7372610a272e839
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-01-24 13:53:09 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     "https://dktheme.online",
         "https://filmyking.site",
         "https://bollypro.sbs",
-        "https://filmyking.rg.gd",
+        "https://filmyking.rf.gd",
         "https://sauthflix.com",
         "https://moviemod.city",
         "https://sauthmovies.com",
