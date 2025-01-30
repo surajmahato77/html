e7f63931cd3d57e317b2cad715da14bfc06a9b97
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-01-30 13:55:56 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "https://netprimemovies1.blogspot.com/",
         "https://filmyking.site",
         "https://bollypro.sbs",
+        "https://vegamoviese.fun",
         "https://www.netprimemovies.site",
         "https://www.cineflim.com",
         "https://abc2movie.blogspot.com",
