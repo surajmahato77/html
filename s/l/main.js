1a16f7dccfecfb8b0458f0a20b63bb45e10bd0f3
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-13 19:34:00 +0530

    Update main.js

--- s/l/main.js
+++ s/l/main.js
@@ -1,1 +1,9 @@
 
+window.onload = function () {
+    setTimeout(function () {
+        var container = document.querySelector('.container');
+        if (container) {
+            container.style.display = 'block';
+        }
+    }, 10); // Optional delay (100ms) for better rendering
+};
