d77e0747bb148ab35e0c861137e97984ea14c364
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-13 19:38:32 +0530

    Update main.js

--- s/l/main.js
+++ s/l/main.js
@@ -1,9 +1,6 @@
-
-window.onload = function () {
-    setTimeout(function () {
-        var container = document.querySelector('.container');
-        if (container) {
-            container.style.display = 'block';
-        }
-    }, 10); // Optional delay (100ms) for better rendering
-};
+document.addEventListener("DOMContentLoaded", function () {
+    var container = document.querySelector('.container');
+    if (container) {
+        container.style.setProperty("display", "block", "important");
+    }
+});
