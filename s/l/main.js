1110016a4b41c75d88890bd3174e25226d718a8e
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-13 19:40:45 +0530

    Update main.js

--- s/l/main.js
+++ s/l/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var container = document.querySelector('.container');
+    var container = document.querySelector('#main');
     if (container) {
         container.style.setProperty("display", "block", "important");
     }
