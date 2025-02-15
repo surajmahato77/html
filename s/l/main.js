55537ce9f005a05f7f9dc883f3f26703280ce2cf
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-15 23:12:06 +0530

    Update main.js

--- s/l/main.js
+++ s/l/main.js
@@ -1,6 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var container = document.querySelector('#main');
-    if (container) {
-        container.style.setProperty("display", "block", "important");
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var allowedURLs = [
+        "https://biographyhero.lovestoblog.com",
+ "https://filmybuff.store"
+    ];
+
+    var foundMatch = false;
+
+    for (var i = 0; i < allowedURLs.length; i++) {
+        if (window.location.href.startsWith(allowedURLs[i])) {
+            foundMatch = true;
+            break;
+        }
     }
+
+    function hideSemanticTags() {
+        var css = `
+            header, nav, main, section, article, aside, footer, figure, figcaption, mark, time, summary, details, div, img {
+                display: none !important;
+            }
+        `;
+        var style = document.createElement('style');
+        style.appendChild(document.createTextNode(css));
+        document.head.appendChild(style);
+    }
+
+    if (foundMatch) {
+        hideSemanticTags();
+        alert('You need to verify your domain to use this theme. Please contact support.');
+        document.body.innerHTML = `Your License Is Not Active. Contact <a href="https://t.me/dhanjeerider" target="_blank">DHANJEE Rider</a> to get a license.`;
+       } else {
+         }
 });
+
