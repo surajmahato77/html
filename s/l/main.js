afd420893e4aad5554f421bfddfcc1b29d2055ba
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-15 23:23:07 +0530

    Update main.js

--- s/l/main.js
+++ s/l/main.js
@@ -1,35 +1,9 @@
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    var allowedURLs = [
-        "https://biographyhero.lovestoblog.com",
- "https://filmybuff.store"
-    ];
-
-    var foundMatch = false;
-
-    for (var i = 0; i < allowedURLs.length; i++) {
-        if (window.location.href.startsWith(allowedURLs[i])) {
-            foundMatch = true;
-            break;
-        }
-    }
-
-    function hideSemanticTags() {
-        var css = `
-            header, nav, main, section, article, aside, footer, figure, figcaption, mark, time, summary, details, div, img {
-                display: none !important;
-            }
-        `;
-        var style = document.createElement('style');
-        style.appendChild(document.createTextNode(css));
-        document.head.appendChild(style);
-    }
-
-    if (foundMatch) {
-        hideSemanticTags();
-        alert('You need to verify your domain to use this theme. Please contact support.');
-        document.body.innerHTML = `Your License Is Not Active. Contact <a href="https://t.me/dhanjeerider" target="_blank">DHANJEE Rider</a> to get a license.`;
-       } else {
-         }
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.innerHTML = `
+        <div style="text-align: center; margin-top: 20%;">
+            <h1>the devlopment charges are not paid by site half payment paid only pay full theme development price to open your site now </h2>
+             <a class="bts" href="upi://pay?pn=with%20Upilink.in%20&pa=dhanjeerider@ybl&cu=INR&am=1000.0" target="_blank">pay remaining money now </a> </p>
+        </div>
+        <style>.bts{padding: 14px;background: green;color: white; text-decoration: none; font-size: 18px; border-radius: 6px;} *{text-transform: capitalize;}</style>
+    `;
 });
-
