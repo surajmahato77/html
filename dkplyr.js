da80ac39a4bfd48a190fdf51216f13fedab88cf1
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-17 11:34:04 +0530

    Update dkplyr.js

--- dkplyr.js
+++ dkplyr.js
@@ -1,12 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
     const playerContainer = document.getElementById(IndStreamPlayerConfigs.id);
     if (playerContainer) {
-      const iframe = document.createElement('iframe');
-      iframe.width = `${IndStreamPlayerConfigs.width}px`;
-      iframe.height = `${IndStreamPlayerConfigs.height}px`;
-      iframe.src = `https://kioled326aps.com/play/${IndStreamPlayerConfigs.src}`;
-      iframe.setAttribute('frameborder', '0');
-      iframe.setAttribute('allowfullscreen', 'true');
-      playerContainer.appendChild(iframe);
-    }
+    const wrapper = document.createElement('div');
+    wrapper.style.position = 'relative';
+    wrapper.style.width = '100%';
+    wrapper.style.paddingTop = '56.25%'; // 16:9 Aspect Ratio
+
+    const iframe = document.createElement('iframe');
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+
+    iframe.src = `https://kioled326aps.com/play/${IndStreamPlayerConfigs.src}`;
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allowfullscreen', 'true');
+
+    wrapper.appendChild(iframe);
+    playerContainer.appendChild(wrapper);
+}
 });
