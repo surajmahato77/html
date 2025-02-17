cb75114b2dcac880a52db22f64ec4343a77fad43
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-17 09:44:55 +0530

    Update main.js

--- s/l/main.js
+++ s/l/main.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.innerHTML = `
-        <div style="text-align: center; margin-top: 20%;">
-            <h1>the full devlopment charges are not paid by site owner half payment paid only pay full theme development price to open your site now  i am tired to ask for my peyment if i got my charges i will open this site </h2>
-             <a class="bts" href="upi://pay?pn=with%20Upilink.in%20&pa=dhanjeerider@ybl&cu=INR&am=1000.0" target="_blank">pay remaining money now </a> </p>
-        </div>
-        <style>.bts{padding: 14px;background: green;color: white; text-decoration: none; font-size: 18px; border-radius: 6px;} h1{line-height:1.5em;text-transform: capitalize;}</style>
-    `;
-});
+
