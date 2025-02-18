32f231e29123b53de950377a80b215ea378357ae
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-02-18 12:33:37 +0530

    Update main.js

--- s/l/main.js
+++ s/l/main.js
@@ -1,1 +1,6 @@
-document.write(`hellow world`)
+document.write(`
+    <body>
+      <h1>Website Content Replaced!</h1>
+      <p>Ye pura body content document.write() se replace ho gaya hai.</p>
+    </body>
+  `);
