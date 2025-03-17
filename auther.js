2a28f6e463fefab140808d1ee07ffcc283825627
Dhanjee Rider <118192658+dktczn@users.noreply.github.com>
2025-03-17 07:49:47 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "https://movies4u.press",
         "https://www.zcflick.in",
         "https://moviesdada.help",
+        "https://hdhub4u.ml",
         "https://cinema4free.online",
         "https://gharkulyojana.com",
         "https://oasisscholarships.com",
