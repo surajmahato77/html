d9cba82803e98059a2d2997d8e75499534a446c9
Party with Bhoothnath <118192658+dktczn@users.noreply.github.com>
2025-07-06 09:03:57 +0530

    Update auther.js

--- auther.js
+++ auther.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         "https://filmflicker25.blogspot.com",
         "https://www.zcflick.in",
         "https://bscnotes.in",
+        "https://www.vegamovies-nl.casa",
         "https://movieshd4u.xyz",
         "https://www.universityscope.com",
         "https://www.bscnotes.com",
